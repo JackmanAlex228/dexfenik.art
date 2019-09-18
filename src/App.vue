ced0458faf96e76f66f2bca5f9f9db1464af49bf
Alex Jackman <jackman.alex.w@gmail.com>
2019-09-18 15:24:41 -0600

    Finally figured the button stuff

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,17 @@
   <div id="app">
     <logo/>
     <temp/>
-    <menu/>
   </div>
 </template>
 <script>
 import logo from './components/logo.vue'
 import temp from './components/temp.vue'
-import menu from './components/menu.vue'
 
 export default {
   name: 'app',
   components: {
     logo,
     temp,
-    menu
   }
 }
 </script>
--- src/components/temp.vue
+++ src/components/temp.vue
@@ -1,26 +1,43 @@
 <template>
-  <div id="body">
-    <!-- <img src="../assets/construct_dex.png" height="350px"> -->
-    <h1>Oh...heh...hello there...</h1>
-    <p>This is my official website. As you can see, there's a lot I need to work on.</p>
-    <p>Let's go to my <a href="https://twitter.com/DexFenik">Twitter</a> or <a href="https://www.instagram.com/dexfenik/">Instagram</a> while we wait.</p>
-  </div>
+    <div id="body">
+        <div>
+            <b-button>Gallery</b-button>
+            <b-button>Characters</b-button>
+            <b-button>About</b-button>
+            <b-button>Commissions</b-button>
+            <b-button>Contact</b-button>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'temp',
-  props: {
-    msg: String
-  }
+    name: 'temp',
+    props: {
+        msg: String
+    }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  #body {
-    text-align: center;
-    font-family: "Lucida Sans Unicode";
-    color: #5d5d5d;
-  }
+    .btn {
+        color: #5D5D5D;
+        border: none;
+        background: none;
+        height: auto;
+        margin-left: 5px;
+        margin-right: 5px;
+    }
+    .btn:hover {
+        background: none;
+        color: #FF6600;
+    }
+    .btn:active {
+        background: none !important;
+        color: #FF6600 !important;
+    }
+    #body {
+        text-align: center;
+        font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif
+    }
 </style>
