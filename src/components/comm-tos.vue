554db0925787d60d4dee0c1c375b1686f0ecb15a
Alex Jackman <jackman.alex.w@gmail.com>
2019-09-25 17:37:53 -0600

    almost done with the terms of service

--- src/components/comm-tos.vue
+++ src/components/comm-tos.vue
@@ -1,7 +1,32 @@
 <template>
     <div class="body">
+        <h3></h3>
         <h3>What I will not draw</h3>
-        <p></p>
+        <ul>
+            <li>Any fetishes or potential interpretations of fetishes.</li>
+            <li>Anything with malicious intent such as callouts or hate speech.</li>
+            <li>Ideologically sensative material in a serious context such as suicides or substance abuse.</li>
+            <li>Extreme violence in a serious context.</li>
+            <li>Romantic situations for couples I don't know personally.</li>
+            <li>Fanart or art of copyrighted characters.</li>
+        </ul>
+        <h3>What I can draw</h3>
+        <ul>
+            <li>Maws, paws, micro, or macro are okay as long as it's not sexually implicit.</li>
+            <li>Inmodest clothing for cartoony characters such as speedos and bikinis.</li>
+            <li>Subtle SFW nudity, with or without underwear.</li>
+            <li>Romantic situations for couples I know personally.</li>
+            <li>Fanart or art of non-copyrighted characters.</li>
+        </ul>
+        <h3>Personal Style Preferences</h3>
+        <ul>
+            <li>When drawing females without clothes I prefer not to draw human breasts. 
+                To make the character appear female I instead draw more chest fluff, eyelashes, or wider hips.</li>
+            <li>When drawing males without clothes I tend to draw fur tuft at the crotch, but sometimes I don't.</li>
+            <li>When drawing anthropomorphic animals standing upright, I prefer to draw them as toony ferals.</li>
+            <li>When drawing anthropomotphic digitigrade animals standing upright, I prefer to draw their legs in 
+                a toony half-digi half-planty style. Although occasionally I'll draw full digi.</li>
+        </ul>
     </div>
 </template>
 
@@ -16,5 +41,10 @@ export default {
         margin-top: 25px;
         margin-bottom: 25px;
         text-align: center;
+    }
+    .body ul {
+        width: 70%;
+        text-align: left;
+        display: inline-block;
     }
 </style>
